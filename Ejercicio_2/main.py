5ab8763c1419a572dc14f6b0c26a3c591340957c
4rm4ndo097 <luisarmandorizogalvan@gmail.com>
2024-06-22 19:30:14 -0600

    agregar la logica para ordenar los criterios

--- Ejercicio_2/main.py
+++ Ejercicio_2/main.py
@@ -1,4 +1,20 @@
 def ordenar_con_criterios(entry, criteria):
+    """
+    Ordena los elementos de la lista 'entry' según los criterios especificados en 'criteria'.
+
+    Primero filtra los elementos que cumplen con todos los criterios dados en 'criteria',
+    luego ordena estos elementos filtrados por la propiedad 'priority' en orden descendente.
+    Finalmente, coloca los elementos que no cumplen con los criterios al final del resultado,
+    manteniendo su orden original.
+
+    Args:
+    entry (list): Lista de diccionarios representando los elementos a ordenar.
+    criteria (list): Lista de tuplas que especifican los criterios de filtrado. Cada tupla tiene
+                     tres elementos: (propiedad, operador, valor).
+
+    Returns:
+    list: Lista de diccionarios ordenada según los criterios especificados.
+    """
     # Filtrar los elementos que cumplen con los criterios especificados
     filtered = []
     for elem in entry:
@@ -51,73 +67,44 @@ def ordenamiento_manual(arr, key, reverse=False):
     return arr
 
 def main():
-    # Ejemplo de entrada: lista de elementos con propiedades
+     # Ejemplo de entrada: lista de elementos con propiedades
     entry = [
-{'id': 12340, 'weight': 1, 'width': 1, 'height': 1, 'length': 1, 'cost':
-125, 'priority': 2},
-{'id': 12341, 'weight': 1, 'width': 1, 'height': 1, 'length': 1, 'cost':
-127, 'priority': 4},
-{'id': 12342, 'weight': 1, 'width': 1, 'height': 1, 'length': 1, 'cost':
-129, 'priority': 6},
-{'id': 12343, 'weight': 1, 'width': 1, 'height': 1, 'length': 1, 'cost':
-131, 'priority': 0},
-{'id': 12344, 'weight': 1, 'width': 1, 'height': 1, 'length': 1, 'cost':
-133, 'priority': 0},
-{'id': 12345, 'weight': 1, 'width': 1, 'height': 1, 'length': 1, 'cost':
-135, 'priority': 0},
-{'id': 12346, 'weight': 1, 'width': 1, 'height': 1, 'length': 1, 'cost':
-137, 'priority': -1},
-{'id': 12347, 'weight': 1, 'width': 1, 'height': 1, 'length': 1, 'cost':
-139, 'priority': 0},
-{'id': 12348, 'weight': 1, 'width': 1, 'height': 1, 'length': 1, 'cost':
-141, 'priority': 2},
-{'id': 12349, 'weight': 1, 'width': 1, 'height': 1, 'length': 1, 'cost':
-143, 'priority': 0},
-{'id': 12350, 'weight': 2, 'width': 1, 'height': 1, 'length': 1, 'cost':
-145, 'priority': 0},
-{'id': 12351, 'weight': 2, 'width': 1, 'height': 1, 'length': 1, 'cost':
-147, 'priority': 10},
-{'id': 12352, 'weight': 2, 'width': 1, 'height': 1, 'length': 1, 'cost':
-149, 'priority': 0},
-{'id': 12353, 'weight': 2, 'width': 1, 'height': 1, 'length': 1, 'cost':
-151, 'priority': 0},
-{'id': 12354, 'weight': 2, 'width': 1, 'height': 1, 'length': 1, 'cost':
-153, 'priority': 0},
-{'id': 12355, 'weight': 2, 'width': 1, 'height': 1, 'length': 10, 'cost':
-155, 'priority': 0},
-{'id': 12356, 'weight': 2, 'width': 1, 'height': 1, 'length': 10, 'cost':
-157, 'priority': 0},
-{'id': 12357, 'weight': 2, 'width': 1, 'height': 1, 'length': 10, 'cost':
-159, 'priority': 0},
-{'id': 12358, 'weight': 2, 'width': 1, 'height': 1, 'length': 10, 'cost':
-161, 'priority': 0},
-{'id': 12359, 'weight': 2, 'width': 1, 'height': 1, 'length': 10, 'cost':
-135, 'priority': 0},
-{'id': 12360, 'weight': 3, 'width': 1, 'height': 1, 'length': 10, 'cost':
-137, 'priority': 0},
-{'id': 12361, 'weight': 3, 'width': 1, 'height': 1, 'length': 10, 'cost':
-139, 'priority': 0},
-{'id': 12362, 'weight': 3, 'width': 3, 'height': 1, 'length': 10, 'cost':
-141, 'priority': -2},
-{'id': 12363, 'weight': 3, 'width': 3, 'height': 1, 'length': 10, 'cost':
-153, 'priority': -2},
-{'id': 12364, 'weight': 3, 'width': 1, 'height': 1, 'length': 10, 'cost':
-145, 'priority': -6},
-{'id': 12366, 'weight': 3, 'width': 1, 'height': 1, 'length': 10, 'cost':
-147, 'priority': 0},
-{'id': 12367, 'weight': 3, 'width': 1, 'height': 1, 'length': 10, 'cost':
-149, 'priority': 0},
-{'id': 12365, 'weight': 3, 'width': 1, 'height': 1, 'length': 10, 'cost':
-151, 'priority': 2},
-{'id': 12368, 'weight': 3, 'width': 1, 'height': 1, 'length': 10, 'cost':
-181, 'priority': 2},
-{'id': 12369, 'weight': 3, 'width': 1, 'height': 1, 'length': 10, 'cost':
-183, 'priority': 0},
-]
+        {'id': 12340, 'weight': 1, 'width': 1, 'height': 1, 'length': 1, 'cost': 125, 'priority': 2},
+        {'id': 12341, 'weight': 1, 'width': 1, 'height': 1, 'length': 1, 'cost': 127, 'priority': 4},
+        {'id': 12342, 'weight': 1, 'width': 1, 'height': 1, 'length': 1, 'cost': 129, 'priority': 6},
+        {'id': 12343, 'weight': 1, 'width': 1, 'height': 1, 'length': 1, 'cost': 131, 'priority': 0},
+        {'id': 12344, 'weight': 1, 'width': 1, 'height': 1, 'length': 1, 'cost': 133, 'priority': 0},
+        {'id': 12345, 'weight': 1, 'width': 1, 'height': 1, 'length': 1, 'cost': 135, 'priority': 0},
+        {'id': 12346, 'weight': 1, 'width': 1, 'height': 1, 'length': 1, 'cost': 137, 'priority': -1},
+        {'id': 12347, 'weight': 1, 'width': 1, 'height': 1, 'length': 1, 'cost': 139, 'priority': 0},
+        {'id': 12348, 'weight': 1, 'width': 1, 'height': 1, 'length': 1, 'cost': 141, 'priority': 2},
+        {'id': 12349, 'weight': 1, 'width': 1, 'height': 1, 'length': 1, 'cost': 143, 'priority': 0},
+        {'id': 12350, 'weight': 2, 'width': 1, 'height': 1, 'length': 1, 'cost': 145, 'priority': 0},
+        {'id': 12351, 'weight': 2, 'width': 1, 'height': 1, 'length': 1, 'cost': 147, 'priority': 10},
+        {'id': 12352, 'weight': 2, 'width': 1, 'height': 1, 'length': 1, 'cost': 149, 'priority': 0},
+        {'id': 12353, 'weight': 2, 'width': 1, 'height': 1, 'length': 1, 'cost': 151, 'priority': 0},
+        {'id': 12354, 'weight': 2, 'width': 1, 'height': 1, 'length': 1, 'cost': 153, 'priority': 0},
+        {'id': 12355, 'weight': 2, 'width': 1, 'height': 1, 'length': 10, 'cost': 155, 'priority': 0},
+        {'id': 12356, 'weight': 2, 'width': 1, 'height': 1, 'length': 10, 'cost': 157, 'priority': 0},
+        {'id': 12357, 'weight': 2, 'width': 1, 'height': 1, 'length': 10, 'cost': 159, 'priority': 0},
+        {'id': 12358, 'weight': 2, 'width': 1, 'height': 1, 'length': 10, 'cost': 161, 'priority': 0},
+        {'id': 12359, 'weight': 2, 'width': 1, 'height': 1, 'length': 10, 'cost': 135, 'priority': 0},
+        {'id': 12360, 'weight': 3, 'width': 1, 'height': 1, 'length': 10, 'cost': 137, 'priority': 0},
+        {'id': 12361, 'weight': 3, 'width': 1, 'height': 1, 'length': 10, 'cost': 139, 'priority': 0},
+        {'id': 12362, 'weight': 3, 'width': 3, 'height': 1, 'length': 10, 'cost': 141, 'priority': -2},
+        {'id': 12363, 'weight': 3, 'width': 3, 'height': 1, 'length': 10, 'cost': 153, 'priority': -2},
+        {'id': 12364, 'weight': 3, 'width': 1, 'height': 1, 'length': 10, 'cost': 145, 'priority': -6},
+        {'id': 12366, 'weight': 3, 'width': 1, 'height': 1, 'length': 10, 'cost': 147, 'priority': 0},
+        {'id': 12367, 'weight': 3, 'width': 1, 'height': 1, 'length': 10, 'cost': 149, 'priority': 0},
+        {'id': 12365, 'weight': 3, 'width': 1, 'height': 1, 'length': 10, 'cost': 151, 'priority': 2},
+        {'id': 12368, 'weight': 3, 'width': 1, 'height': 1, 'length': 10, 'cost': 181, 'priority': 2},
+        {'id': 12369, 'weight': 3, 'width': 1, 'height': 1, 'length': 10, 'cost': 183, 'priority': 0},
+    ]
     
     # Criterio de filtrado
     criteria2 = [
-        ('weight', '=', 3), ('width','=',3),
+        ('weight', '=', 3),
+        ('width', '=', 3),
     ]
     
     # Llamar a la función para ordenar según los criterios especificados
